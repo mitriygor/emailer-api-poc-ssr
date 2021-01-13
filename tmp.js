5a7cbf4923f84c46fce3837ef84488d922dc0b24
Mitry Gorbaciov <dgorbaciov@wandrian.com>
2021-01-12 21:37:36 -0500

    adding loop to the email

---
+++ tmp.js
@@ -0,0 +1,20 @@
+// const getFetch = () => {
+//     return fetch('https://jsonplaceholder.typicode.com/todos/1', {
+//         method: 'GET',
+//         headers: {
+//             Accept: 'application/json',
+//         }
+//     }).then(response => {
+//             if (response.status !== 200) {
+//                 res.status(response.status)
+//                 res.send(err);
+//             } else {
+
+//             }
+//         }).catch((err) => {
+//             res.status(500)
+//             res.send(err);
+//         });
+// }
+
+// let res = getFetch()
